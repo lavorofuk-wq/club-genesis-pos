fecb65e49f56975a2bd71b13429847a576f87e0a
lavorofuk-wq <lavorofuk@gmail.com>
2026-07-22 06:05:20 +0900

    Fix recurring single charge extension blocks

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'genesis-pos-v6.99-no-login';
-const ASSETS = ['./', './index.html', './styles.css', './boot-compat.js?v=6.99', './printer-loader.js?v=6.99', './firebase-init.js?v=6.99', './app.js?v=6.99', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'genesis-pos-v6.100-no-login';
+const ASSETS = ['./', './index.html', './styles.css', './boot-compat.js?v=6.100', './printer-loader.js?v=6.100', './firebase-init.js?v=6.100', './app.js?v=6.100', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
